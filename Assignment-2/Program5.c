b0ca1231c5163bff529e7214aac24f519d15b856
Abhay <abhaychilka18@gmail.com>
2026-01-03 22:26:02 +0530

    2.4,2.5

---
+++ Assignment-2/Program5.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+//accept number and check whether number is even or odd
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkEven(int iNo)
+{
+    if (iNo % 2 == 0)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }   
+}
+
+int main()
+{
+    int iValue = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter number :");
+    scanf("%d", &iValue);
+
+    bRet = ChkEven(iValue);
+
+    if (bRet == TRUE)
+    {
+        printf("Number is even");
+    }
+    else
+    {
+        printf("Number is odd");
+    }
+    return(0);
+}
